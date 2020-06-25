36f434b08e534e2389df1dc0003378888133c23e
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-25 21:20:49 +0000

    added map var

--- project/src/main/webapp/match-script.js
+++ project/src/main/webapp/match-script.js
@@ -14,9 +14,10 @@
 
 /** Creates a map and adds it to the page. */
 function createMap() {
-  new google.maps.Map(document.getElementById('map'), {
+  const map =new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.422, lng: -122.084},
     zoom: 16,
   });
+  return map;
 }
 window.onload = createMap();
