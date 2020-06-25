0037bdb01f34ce4adea0d48c1f65b31b6e9eb4ac
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-25 21:32:21 +0000

    const google

--- project/src/main/webapp/match-script.js
+++ project/src/main/webapp/match-script.js
@@ -14,6 +14,7 @@
 
 /** Creates a map and adds it to the page. */
 function createMap() {
+  const google = window.google;
   const map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.422, lng: -122.084},
     zoom: 16,
