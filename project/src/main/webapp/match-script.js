e1ebe12c6ee9c458d741f4e00a25087532334109
Miah Sanchez <miahsanchez4@gmail.com>
2020-06-25 21:35:35 +0000

    deleted return statement

--- project/src/main/webapp/match-script.js
+++ project/src/main/webapp/match-script.js
@@ -15,10 +15,9 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
   const google = window.google;
-  const map = new google.maps.Map(document.getElementById('map'), {
+  new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.422, lng: -122.084},
     zoom: 16,
   });
-  return map;
 }
 window.onload = createMap();
