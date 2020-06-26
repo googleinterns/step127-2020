d18f200863be953866a8e4c273175d0418d42e33
Alexander Costa <alexcostaluiz@outlook.com>
2020-06-26 04:39:12 +0000

    Move index files outside of home directory

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -3,8 +3,8 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '../App';
-import * as serviceWorker from '../serviceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
