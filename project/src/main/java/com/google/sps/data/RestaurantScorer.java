503339687e097f9a5a44db4f80983472c24cdecc
Kira Pancha <kirakpancha@gmail.com>
2020-06-29 14:24:04 -0400

    Add static final vars

--- project/src/main/java/com/google/sps/data/RestaurantScorer.java
+++ project/src/main/java/com/google/sps/data/RestaurantScorer.java
@@ -4,6 +4,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public final class RestaurantScorer {
+  private static final int MAX_RATING = 5;
+  private static final int MID_RATING = 3;
   /** Class should not be instantiated. */
   private RestaurantScorer() {}
 
@@ -22,12 +24,12 @@ public final class RestaurantScorer {
     if (restaurant.getPlaceTypes().contains(preferredDiningExp)) {
       score += diningExpWeight;
     }
-    if (restaurantRating >= 3) {
-      score += restaurantRating / 5;
-    } else if (restaurantRating > 0) {
-      // Subtract more points from score for lower rating. This calculation will be improved.
-      score -= (0.6 - restaurantRating / 5);
+    // This statement executes if a rating exists for the resturant.
+    // Adds points to the score if the rating is > 3, subtracts points if rating is < 3.
+    if (restaurantRating > 0) {
+      score += (restaurantRating / MAX_RATING) - (MID_RATING / MAX_RATING);
     }
+
     double percentMatch = score / maxPoints;
     return percentMatch;
   }
