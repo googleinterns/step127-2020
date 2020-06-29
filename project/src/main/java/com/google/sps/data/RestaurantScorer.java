5ac9bfeb20539312eeee53128b04ee40b7776ece
Kira Pancha <kirakpancha@gmail.com>
2020-06-29 17:15:00 -0400

    Improve var names

--- project/src/main/java/com/google/sps/data/RestaurantScorer.java
+++ project/src/main/java/com/google/sps/data/RestaurantScorer.java
@@ -19,20 +19,23 @@ public final class RestaurantScorer {
     int diningExpWeight = preferences.getJSONObject("diningExp").getInt("weight");
     int radiusWeight = preferences.getJSONObject("radius").getInt("weight");
     int maxPoints = priceLevelWeight + diningExpWeight + radiusWeight + 1;
-    Map<String, Double> currLocation = new Gson().fromJson(preferences.getJSONObject("currLocation").toString(), HashMap.class);
+    Map<String, Double> currLocation =
+        new Gson().fromJson(preferences.getJSONObject("currLocation").toString(), HashMap.class);
     double score = 0;
     double restaurantRating = restaurant.getAvgRating();
 
     if (restaurant.getPriceLevel() == preferences.getJSONObject("priceLevel").getInt("pref")) {
       score += priceLevelWeight;
     }
-    if (restaurant.getPlaceTypes().contains(preferences.getJSONObject("diningExp").getString("pref"))) {
+    if (restaurant.getPlaceTypes().contains(
+            preferences.getJSONObject("diningExp").getString("pref"))) {
       score += diningExpWeight;
     }
-    if (distanceInMeters(currLocation, restaurant.getLatLngCoords()) <= preferences.getJSONObject("radius").getInt("pref")) {
+    if (distanceInMeters(currLocation, restaurant.getLatLngCoords())
+        <= preferences.getJSONObject("radius").getInt("pref")) {
       score += radiusWeight;
     }
-    // Adds points to the score if the rating is > MID_RATING, subtracts points if 
+    // Adds points to the score if the rating is > MID_RATING, subtracts points if
     // rating is < MID_RATING.
     boolean hasRating = restaurantRating != -1;
     if (hasRating) {
@@ -43,14 +46,15 @@ public final class RestaurantScorer {
   }
 
   /** Calculates the distance in meters between two sets of lat long coordinates. */
-  public static double distanceInMeters(Map<String,Double> currCoords, Map<String,Double> placeCoords) {
+  public static double distanceInMeters(
+      Map<String, Double> currCoords, Map<String, Double> placeCoords) {
     double latDiff = Math.toRadians(placeCoords.get("lat") - currCoords.get("lat"));
     double lngDiff = Math.toRadians(placeCoords.get("lng") - currCoords.get("lng"));
-    double a = Math.sin(latDiff/2) * Math.sin(latDiff/2) +
-        Math.cos(Math.toRadians(currCoords.get("lat"))) * Math.cos(Math.toRadians(placeCoords.get("lat"))) *
-        Math.sin(lngDiff/2) * Math.sin(lngDiff/2);
-    double distInRadians = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    double distInMeters = EARTH_RADIUS * distInRadians;
+    double point = Math.pow(Math.sin(latDiff / 2), 2)
+        + Math.cos(Math.toRadians(currCoords.get("lat")))
+            * Math.cos(Math.toRadians(placeCoords.get("lat"))) * Math.pow(Math.sin(lngDiff / 2), 2);
+    double angleInRadians = 2 * Math.atan2(Math.sqrt(point), Math.sqrt(1 - point));
+    double distInMeters = EARTH_RADIUS * angleInRadians;
     return distInMeters;
   }
 }
