72a83ccf23f42d66c830070b581e78fe74f87c64
Kira Pancha <kirakpancha@gmail.com>
2020-06-30 10:01:24 -0400

    Subtract from score for out of range places

--- project/src/main/java/com/google/sps/data/RestaurantScorer.java
+++ project/src/main/java/com/google/sps/data/RestaurantScorer.java
@@ -7,7 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public final class RestaurantScorer {
-  private static final double EARTH_RADIUS = 6371000; // meters
+  private static final double EARTH_RADIUS_METERS = 6371000;
   private static final int MAX_RATING = 5;
   private static final int MID_RATING = 3;
   /** Class should not be instantiated. */
@@ -23,6 +23,8 @@ public final class RestaurantScorer {
         new Gson().fromJson(preferences.getJSONObject("currLocation").toString(), HashMap.class);
     double score = 0;
     double restaurantRating = restaurant.getAvgRating();
+    double distMeters = distanceInMeters(currLocation, restaurant.getLatLngCoords());
+    double prefDistMeters = preferences.getJSONObject("radius").getDouble("pref");
 
     if (restaurant.getPriceLevel() == preferences.getJSONObject("priceLevel").getInt("pref")) {
       score += priceLevelWeight;
@@ -31,9 +33,12 @@ public final class RestaurantScorer {
             preferences.getJSONObject("diningExp").getString("pref"))) {
       score += diningExpWeight;
     }
-    if (distanceInMeters(currLocation, restaurant.getLatLngCoords())
-        <= preferences.getJSONObject("radius").getInt("pref")) {
+    if (distMeters <= prefDistMeters) {
       score += radiusWeight;
+    } else {
+      double percentDistDiff = (roundMetersToMiles(distMeters) - roundMetersToMiles(prefDistMeters))
+          / roundMetersToMiles(prefDistMeters);
+      score -= percentDistDiff * radiusWeight;
     }
     // Adds points to the score if the rating is > MID_RATING, subtracts points if
     // rating is < MID_RATING.
@@ -46,7 +51,7 @@ public final class RestaurantScorer {
   }
 
   /** Calculates the distance in meters between two sets of lat long coordinates. */
-  public static double distanceInMeters(
+  private static double distanceInMeters(
       Map<String, Double> currCoords, Map<String, Double> placeCoords) {
     double latDiff = Math.toRadians(placeCoords.get("lat") - currCoords.get("lat"));
     double lngDiff = Math.toRadians(placeCoords.get("lng") - currCoords.get("lng"));
@@ -54,7 +59,12 @@ public final class RestaurantScorer {
         + Math.cos(Math.toRadians(currCoords.get("lat")))
             * Math.cos(Math.toRadians(placeCoords.get("lat"))) * Math.pow(Math.sin(lngDiff / 2), 2);
     double angleInRadians = 2 * Math.atan2(Math.sqrt(point), Math.sqrt(1 - point));
-    double distInMeters = EARTH_RADIUS * angleInRadians;
+    double distInMeters = EARTH_RADIUS_METERS * angleInRadians;
     return distInMeters;
+  }
+
+  /** Converts meters to miles and rounds to the nearest mile. */
+  private static long roundMetersToMiles(double numMeters) {
+    return Math.round(numMeters / 1609.34);
   }
 }
