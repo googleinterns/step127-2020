34d18c9637ffa99d6a75c3109abe8ff4039cd0b4
Miah Sanchez <miahsanchez@google.com>
2020-06-29 23:20:35 -0400

    changed the option values using map

--- project/frontend/src/FindMatch/FindMatch.js
+++ project/frontend/src/FindMatch/FindMatch.js
@@ -6,35 +6,29 @@ class PreferenceForm extends React.Component {
     super(props)
   }
   render() {
-    const cuisines = ["Italian", "Mexican", "Indian"]
+    const cuisines = ["Italian", "Mexican", "Indian"];
+    const distances = ["1 mile", "5 miles", "10 miles", "25 miles"];
+    const dining_experiences = ["Takeout", "Delivery", "Dine-In"];
+    const price_levels = ["Low", "Medium", "High"];
     return (
       <form id="restaurant-prefs">
         <label for="cuisine">What cuisine?</label>
         <select name="cuisine" id="cuisine">
-          <option value="Italian">Italian</option>
-          <option value="Mexican">Mexican</option>
-          <option value="Indian">Indian</option>
+          {cuisines.map(cuisine => <option value={cuisine}>{cuisine}</option>)};
         </select>
         <label for="distance">Distance</label>
         <select name="distance" id="distance">
-          <option value="1 mile">1 mile</option>
-          <option value="5 miles">5 miles</option>
-          <option value="10 miles">10 miles</option>
-          <option value="25 miles">25 miles</option>
+          {distances.map(distance => <option value={distance}>{distance}</option>)};
         </select>
         <br />
         <label for="dining-experience">Dining Experience</label>
         <select name="dining-experience" id="dining-experience">
-          <option value="takeout">Takeout</option>
-          <option value="delivery">Delivery</option>
-          <option value="dine-in">Dine-In</option>
+          {dining_experiences.map(experience => <option value={experience}>{experience}</option>)};
         </select>
         <br />
         <label for="price-level">Price</label>
         <select name="price-level" id="price-level">
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
+          {price_levels.map(price => <option value={price}>{price}</option>)};
         </select>
         <br />
         <label for="latitude">Latitude</label>
