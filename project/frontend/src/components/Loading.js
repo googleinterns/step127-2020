d3f00a1396434e763ed1b337f58628c9c5394e7e
Alexander Costa <alexcostaluiz@outlook.com>
2020-06-30 02:55:48 +0000

    Extract loading icon into its own component

---
+++ project/frontend/src/components/Loading.js
@@ -0,0 +1,17 @@
+import './Loading.css';
+
+import React from 'react';
+
+/**
+ * An animated loading icon.
+ */
+function Loading(props) {
+  return (
+    <div id="loading-comments" className="loading-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Loading;
