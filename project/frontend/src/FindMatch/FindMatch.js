39f4d9ee08eb561a2b983eb7993f95f760b9228d
Miah Sanchez <miahsanchez@google.com>
2020-07-01 13:11:17 -0400

    add handleSubmit

--- project/frontend/src/FindMatch/FindMatch.js
+++ project/frontend/src/FindMatch/FindMatch.js
@@ -6,6 +6,7 @@ class PreferenceForm extends React.Component {
     super(props)
     this.state = { cuisine: "" }
     this.changeState = this.changeState.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   // Updates the state of the input element so it holds the chosen value.
@@ -13,10 +14,14 @@ class PreferenceForm extends React.Component {
     this.setState({ cuisine: event.target.value })
   }
 
+  handleSubmit(event) {
+    alert("the cuisine you chose is: " + this.state.cuisine)
+  }
+
   render() {
     const cuisines = ["italian", "mexican", "indian"]
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>
           What cuisine?
           <select
