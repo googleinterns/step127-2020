c4e47e30d224bbefdc67bf7dde482426ff65267f
Kira Pancha <kirakpancha@gmail.com>
2020-07-06 14:39:46 -0400

    Add lint indent comment

--- project/backend/src/main/webapp/script.js
+++ project/backend/src/main/webapp/script.js
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* eslint indent: ["error", 2, { "MemberExpression": 1 }]*/
 /* exported getRecommendation */
 function getRecommendation() {
   const cuisineType = document.getElementById('cuisine').value;
   const radius = milesToMeters(
-      parseInt(document.getElementById('distance').value),
+    parseInt(document.getElementById('distance').value),
   );
   const priceLevel = parseInt(document.getElementById('price-level').value);
   const lat = parseFloat(document.getElementById('latitude').value);
@@ -35,48 +36,48 @@ function getRecommendation() {
   searchParams.append('radius', radius);
   searchParams.append('key', apiKey);
   fetch(proxyUrl + textSearchBaseUrl + searchParams)
-  // This gives us the list of restaurants.
-      .then((response) => response.json())
-      .then((data) => {
-        const restaurants = data.results;
-        fetch('/recommendation', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            restaurants,
-            preferences: {
-              cuisineType,
-              currLocation: {
-                lat,
-                lng,
-              },
-              radius: {
-                pref: radius,
-                weight: radiusWeight,
-              },
-              priceLevel: {
-                pref: priceLevel,
-                weight: priceLevelWeight,
-              },
-              diningExp: {
-                pref: diningExp,
-                weight: diningExpWeight,
-              },
+    // This gives us the list of restaurants.
+    .then((response) => response.json())
+    .then((data) => {
+      const restaurants = data.results;
+      fetch('/recommendation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          restaurants,
+          preferences: {
+            cuisineType,
+            currLocation: {
+              lat,
+              lng,
             },
-          }),
-        })
-            .then((response) => response.text())
-            .then((data) => {
-              try {
-                const selections = JSON.parse(data);
-                console.log(selections);
-              } catch (err) {
-                console.log(err);
-              }
-            });
-      });
+            radius: {
+              pref: radius,
+              weight: radiusWeight,
+            },
+            priceLevel: {
+              pref: priceLevel,
+              weight: priceLevelWeight,
+            },
+            diningExp: {
+              pref: diningExp,
+              weight: diningExpWeight,
+            },
+          },
+        }),
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          try {
+            const selections = JSON.parse(data);
+            console.log(selections);
+          } catch (err) {
+            console.log(err);
+          }
+        });
+    });
 }
 
 function milesToMeters(numMiles) {
