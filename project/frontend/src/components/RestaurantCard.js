002a41fe0fde4134b7adb6ed84308bbd099fd9fc
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-07 16:55:23 +0000

    Retrieve data from props to populate RestaurantCard component

--- project/frontend/src/components/RestaurantCard.js
+++ project/frontend/src/components/RestaurantCard.js
@@ -9,13 +9,12 @@ import HalfStar from '../assets/star_half.svg';
 import EmptyStar from '../assets/star_border.svg';
 import Place from '../assets/place.svg';
 import Globe from '../assets/globe.svg';
+import Phone from '../assets/phone.svg';
+import Clock from '../assets/clock.svg';
 
 function RatingStars(props) {
-  let average = '4.5'; // props.average;
-  const numRatings = '213'; // props.numRatings;
-
   const stars = [];
-  for (let average = '4.5'; average > 0; average--) {
+  for (let average = props.avgRating; average > 0; average--) {
     stars.push((average >= 1) ? 1 : 0.5);
   }
   while (stars.length < 5) {
@@ -31,28 +30,44 @@ function RatingStars(props) {
           height='15'
         />
       ))}
-      <span>{average} ({numRatings})</span>
+      <span>{props.avgRating} ({props.numRatings})</span>
     </div>
   );
 }
 
 function RestaurantCard(props) {
+  const restaurant = props.restaurant;
+  const details = props.details;
+  
   const images = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5'];
 
   return (
     <div className='restaurant-card'>
       <ImageSlider images={images} />
       <div className='restaurant-content'>
-        <h5 className='restaurant-name'>Cafe Badilico</h5>
-        <RatingStars />
-        <p className='restaurant-type'>$$ • Italian, Cafe</p>
+        <h5 className='restaurant-name'>{restaurant.key.name}</h5>
+        <RatingStars
+          avgRating={restaurant.key.avgRating}
+          numRatings={restaurant.key.numRatings}
+        />
+        <p className='restaurant-type'>
+          {'$'.repeat(restaurant.key.priceLevel)} • {restaurant.key.placeTypes.join(', ')}
+        </p>
+        <div className='restaurant-detail-container'>
+          <img src={Clock} />
+          <span>{details.result.opening_hours.open_now ? 'Open' : 'Closed'}</span>
+        </div>
         <div className='restaurant-detail-container'>
           <img src={Place} />
-          <span>123 Pinewood Street, Atlanta GA, 30022</span>
+          <span>{restaurant.key.address}</span>
+        </div>
+        <div className='restaurant-detail-container'>
+          <img src={Phone} />
+          <span>{details.result.formatted_phone_number}</span>
         </div>
         <div className='restaurant-detail-container'>
           <img src={Globe} />
-          <span>https://restaurantwebsite.com</span>
+          <span>{details.result.website}</span>
         </div>
       </div>
     </div>
