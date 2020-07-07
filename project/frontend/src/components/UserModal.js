1e98e22a020afdebac23ece7147bb19828a63234
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-07 22:04:23 +0000

    Pull props fields out of props

--- project/frontend/src/components/UserModal.js
+++ project/frontend/src/components/UserModal.js
@@ -5,22 +5,24 @@ import React from 'react';
 import Modal from '../components/Modal.js';
 
 function UserModal(props) {
+  const {isModalOpen, toggleModal, user, signOut} = props;
+  
   return (
     <Modal
-      open={props.isModalOpen}
-      onDismiss={props.toggleModal}
+      open={isModalOpen}
+      onDismiss={toggleModal}
       top='64px'
       right='16px'>
       <img
         className='profile-pic large'
-        src={props.user.getImageUrl()}
+        src={user.getImageUrl()}
         alt='Large profile.'
       />
-      <h4 className='user-name'>{props.user.getName()}</h4>
-      <h5 className='user-email'>{props.user.getEmail()}</h5>
+      <h4 className='user-name'>{user.getName()}</h4>
+      <h5 className='user-email'>{user.getEmail()}</h5>
       <button>Profile</button>
       <br />
-      <button className='sign-out' onClick={props.signOut}>
+      <button className='sign-out' onClick={signOut}>
         Sign Out
       </button>
     </Modal>
