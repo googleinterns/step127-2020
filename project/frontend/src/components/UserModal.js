049e32557789d328a6490e18e22ead86d13942b6
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-07 22:30:25 +0000

    Add documentation for UserModal component

--- project/frontend/src/components/UserModal.js
+++ project/frontend/src/components/UserModal.js
@@ -4,11 +4,24 @@ import React from 'react';
 
 import Modal from '../components/Modal.js';
 
+/**
+ * A Modal implementation which displays the specified user information.
+ * 
+ * @param {bool} props.open True if the modal should be shown; false if it
+ *     should be hidden.
+ * @param {function(): undefined} props.onDismiss Callback for when modal 
+ *     is dismissed by touching modal backdrop, should update props.open
+ *     to false.
+ * @param {!gapi.auth2.BasicProfile} user The basic profile of the user
+ *     whose information should be displayed.
+ * @param {function(): undefined} signOut A callback for when the sign out
+ *     button is pressed.
+ */
 function UserModal(props) {
-  const { isModalOpen, toggleModal, user, signOut } = props;
+  const { open, onDismiss, user, signOut } = props;
 
   return (
-    <Modal open={isModalOpen} onDismiss={toggleModal} top='64px' right='16px'>
+    <Modal open={open} onDismiss={onDismiss} top='64px' right='16px'>
       <img
         className='profile-pic large'
         src={user.getImageUrl()}
