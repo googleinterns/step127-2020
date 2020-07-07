7684cda369b1da09d4c8edaaf0ad7c497c00d40f
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-07 21:02:53 +0000

    Remove signedIn state and refactor currentUser state

--- project/frontend/src/components/Authentication.js
+++ project/frontend/src/components/Authentication.js
@@ -3,46 +3,29 @@ import React, { useState, useEffect } from 'react';
 const AuthContext = React.createContext({});
 
 function Authentication(props) {
-  const [GoogleAuth, setGoogleAuth] = useState(undefined);
-  const [currentUser, setCurrentUser] = useState(undefined);
-  const [signedIn, setSignedIn] = useState(false);
-
-  const context = {
-    GoogleAuth: { get: GoogleAuth, set: setGoogleAuth },
-    currentUser: { get: currentUser, set: setCurrentUser },
-    signedIn: { get: signedIn, set: setSignedIn },
-  };
+  const [GoogleAuth, setGoogleAuth] = useState({signIn: () => {}, signOut: () => {}});
+  const [currentUser, setCurrentUser] = useState({get: undefined});
 
   useEffect(() => {
-    const onGoogleAuthLoaded = () => {
-      window.gapi.load('auth2', () => {
-        const auth2 = window.gapi.auth2.init({
-          client_id:
-            '21423804760-e3goj1cdhg49ojdf780mcq92qgshbr4v.apps.googleusercontent.com',
-        });
-
-        auth2.isSignedIn.listen((signedIn) => {
-          setSignedIn(signedIn);
-        });
-
-        auth2.currentUser.listen((user) => {
-          setCurrentUser(user);
-        });
-
-        setGoogleAuth(auth2);
+    window.gapi.load('auth2', () => {
+      const auth2 = window.gapi.auth2.init({
+        client_id:
+        '21423804760-e3goj1cdhg49ojdf780mcq92qgshbr4v.apps.googleusercontent.com',
       });
-    };
 
-    if (window.gapi) {
-      onGoogleAuthLoaded();
-      return () => {};
-    } else {
-      window.addEventListener('DOMContentLoaded', onGoogleAuthLoaded);
-      return () => {
-        window.removeEventListener('DOMContentLoaded', onGoogleAuthLoaded);
-      };
-    }
+      auth2.currentUser.listen((user) => {
+        setCurrentUser({get: user});
+      });
+
+      setGoogleAuth(auth2); 
+    });
   }, []);
+  
+  const context = {
+    currentUser: currentUser,
+    signIn: GoogleAuth.signIn,
+    signOut: GoogleAuth.signOut,
+  };
 
   return (
     <AuthContext.Provider value={context}>
