2489581a5a96316dad18352972fc480ce8853bfd
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-07 21:57:28 +0000

    Add documentation to Authentication.js

--- project/frontend/src/components/Authentication.js
+++ project/frontend/src/components/Authentication.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
+/**
+ * A context containing the following fields:
+ * - currentUser {!GoogleUser}
+ * - signIn {function(): undefined}
+ * - signOut {function(): undefined}
+ *
+ * Components that need authentication information may listen to this
+ * context using the useContext(AuthContext) hook, AuthContext.Consumer
+ * component, or the ClassWidget.contextType property.
+ */
 const AuthContext = React.createContext({});
 
+/**
+ * A high-order component responsible for initializing and maintaining authentication state
+ * within the application. Serves the AuthContext to child components.
+ */
 function Authentication(props) {
   const [GoogleAuth, setGoogleAuth] = useState({
     signIn: () => {},
