cef5ba75a8413b9476ab590734aed89236577e07
Kira Pancha <kirakpancha@gmail.com>
2020-07-08 08:50:06 -0400

    Run prettier

--- project/frontend/src/pages/HomePage.js
+++ project/frontend/src/pages/HomePage.js
@@ -12,7 +12,9 @@ function HomePage() {
           <button>Location</button>
           <div>
             <button>Swipe Match</button>
-            <button onClick={() =>  window.location.href='/find-match'}>Find My Match</button>
+            <button onClick={() => (window.location.href = '/find-match')}>
+              Find My Match
+            </button>
           </div>
         </div>
       </div>
