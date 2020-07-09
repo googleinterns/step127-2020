7302b704bc1f62982b53bdea83a843d05b7ce5c7
Miah Sanchez <miahsanchez@google.com>
2020-07-09 16:39:45 -0400

    beginning to change to functional component

--- project/frontend/src/ResultsMap.js
+++ project/frontend/src/ResultsMap.js
@@ -7,22 +7,21 @@ const mapStyle = {
   width: '500px',
 };
 
-export class MapContainer extends React.Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyle}
-        initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233,
-        }}
-      />
-    );
-  }
+function MapContainer(props) {
+  const coords = {lat:, lng: };
+  return (
+    <Map
+      google={props.google}
+      zoom={14}
+      style={mapStyle}
+      center={{
+        lat: -1.2884,
+        lng: 36.8233,
+      }}
+    />
+  );
 }
 
 export default GoogleApiWrapper({
-  apiKey: ProcessingInstruction.env.REACT_APP_GOOGLE_API_KEY,
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
 })(MapContainer);
