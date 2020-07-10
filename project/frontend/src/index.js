44055a1ad256bb8bb75a97e3ce577da235b94fb5
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-10 21:29:52 +0000

    Refactor MatchResultsPage import

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -10,7 +10,7 @@ import Footer from './pages/Footer.js';
 import Header from './pages/Header.js';
 import HomePage from './pages/HomePage.js';
 import MapContainer from './components/ResultsMap.js';
-import MatchResults from './pages/MatchResults.js';
+import MatchResultsPage from './pages/MatchResultsPage.js';
 import PreferenceForm from './components/PreferenceForm.js';
 
 import * as serviceWorker from './serviceWorker';
