6cfd1ef8a3ef0e9a1642f3b1adf0fa738c0dbe47
Miah Sanchez <miahsanchez@google.com>
2020-07-10 14:43:51 -0400

    marker automatically shows up and infoWindow appears on click

--- project/frontend/src/components/ResultsMap.js
+++ project/frontend/src/components/ResultsMap.js
@@ -11,14 +11,21 @@ function MapContainer(props) {
   // Declare all the states for this functional component
   const [activeMarker, changeMarker] = useState({});
   const [showInfoWindow, changeInfoVisibility] = useState(false);
+
   const coords = { lat: 40.837, lng: -73.865 };
+
+  const onMarkerClick = (props, marker, event) => {
+    changeMarker(marker);
+    changeInfoVisibility(true);
+  };
+
   return (
     <Map
       aria-label={'A Google Map with your Matches!'}
       google={props.google}
       zoom={14}
       style={mapStyle}
-      center={coords}>
+      initialCenter={coords}>
       <Marker
         onClick={onMarkerClick}
         name={'Your #1 Match'}
@@ -27,71 +34,15 @@ function MapContainer(props) {
       <InfoWindow
         marker={activeMarker}
         visible={showInfoWindow}
-        onClose={onClose}>
+        onClose={changeInfoVisibility(false)}>
         <div>
-          <h5>{'This is your place'}</h5>
+          <h5>{'Your #1 Match'}</h5>
         </div>
       </InfoWindow>
     </Map>
   );
 }
 
-function onMarkerClick(props, marker, event) {
-  changeMarker(marker);
-  changeInfoVisibility(true);
-}
-
-function onClose(props) {
-  if (showInfoWindow) {
-    changeInfoVisibility(false);
-    changeMarker(null);
-  }
-}
-
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
 })(MapContainer);
-
-// export class MapContainer extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     // TODO: change this to be the first match coordinates
-//     this.state = {
-//       lat: 40.837,
-//       lng: -73.865,
-//       selectedPlace: {},
-//       activeMarker: {},
-//       showingInfoWindow: false,
-//     };
-//   }
-
-//   onClose = (props) => {
-//     if (this.state.showingInfoWindow) {
-//       this.setState({
-//         showingInfoWindow: false,
-//         activeMarker: null,
-//       });
-//     }
-//   };
-
-//   render() {
-//     const { lat, lng } = this.state;
-//     return (
-//       <Map
-//         google={this.props.google}
-//         zoom={14}
-//         style={mapStyle}
-//         center={(lat, lng)}>
-//         <Marker onClick={this.onMarkerClick} name={'Your #1 Match'} />
-//         <InfoWindow
-//           marker={this.state.activeMarker}
-//           visible={this.state.showingInfoWindow}
-//           onClose={this.onClose}>
-//           <div>
-//             <h5>{this.state.selectedPlace.name}</h5>
-//           </div>
-//         </InfoWindow>
-//       </Map>
-//     );
-//   }
-// }
