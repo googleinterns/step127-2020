3a9b5b0fd74ba828a47f72a34015842d61d19c1b
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-10 23:18:50 +0000

    Use prettier on RestaurantCardStack

--- project/frontend/src/components/RestaurantCardStack.js
+++ project/frontend/src/components/RestaurantCardStack.js
@@ -10,17 +10,19 @@ function RestaurantCardStack(props) {
   const stackWrapper = useRef(null);
   const cardHeight = useRef(0);
   const cardRefs = useRef(new Array(props.cards.length));
-  
+
   useLayoutEffect(() => {
     console.log('using layout effect');
     cardHeight.current = cardRefs.current[0].offsetHeight;
-    stackWrapper.current.style.height = (cardHeight.current + 192) + 'px';
-    stackWrapper.current.style.width = cardRefs.current[currentCardIndex].offsetWidth + 'px';
+    stackWrapper.current.style.height = cardHeight.current + 192 + 'px';
+    stackWrapper.current.style.width =
+      cardRefs.current[currentCardIndex].offsetWidth + 'px';
 
     for (let i = currentCardIndex + 1; i < cardRefs.current.length; i++) {
       const card = cardRefs.current[i];
       if (card) {
-        card.style.top = (+card.style.top.slice(0, -2) + cardHeight.current) + 'px';
+        card.style.top =
+          +card.style.top.slice(0, -2) + cardHeight.current + 'px';
       }
     }
   }, []);
@@ -32,15 +34,21 @@ function RestaurantCardStack(props) {
     }
 
     const relativeIndex = index - currentCardIndex;
-    
+
     const style = {
       position: 'absolute',
-      top: (relativeIndex < 5) ? (cardHeight.current + relativeIndex * 24) + 'px' : (cardHeight.current + 72) + 'px',
+      top:
+        relativeIndex < 5
+          ? cardHeight.current + relativeIndex * 24 + 'px'
+          : cardHeight.current + 72 + 'px',
       left: '0px',
-      opacity: (relativeIndex < 4) ? 1 : 0,
+      opacity: relativeIndex < 4 ? 1 : 0,
       zIndex: props.cards.length - index,
-      transform: `scale(${Math.min(1 - (relativeIndex / 20), 1.0)}) rotate(0deg)`,
-      transition: (cardHeight.current === 0.0) ? '' : 'all 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97)',
+      transform: `scale(${Math.min(1 - relativeIndex / 20, 1.0)}) rotate(0deg)`,
+      transition:
+        cardHeight.current === 0.0
+          ? ''
+          : 'all 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97)',
     };
 
     if (relativeIndex === 0) {
@@ -50,12 +58,12 @@ function RestaurantCardStack(props) {
       style.left = '-700px';
       style.transform += ' rotate(-45deg)';
     }
-    
-    const collapsed = (index <= currentCardIndex) ? false : true;
-    
+
+    const collapsed = index <= currentCardIndex ? false : true;
+
     return (
       <RestaurantCard
-        parentRef={(el) => cardRefs.current[index] = el}
+        parentRef={(el) => (cardRefs.current[index] = el)}
         key={card.restaurant.key.id}
         style={style}
         restaurant={card.restaurant}
@@ -71,8 +79,12 @@ function RestaurantCardStack(props) {
         {cards}
       </div>
       <div>
-        <button onClick={() => setCurrentCardIndex((prev) => prev - 1)}>Previous</button>
-        <button onClick={() => setCurrentCardIndex((prev) => prev + 1)}>Next</button>
+        <button onClick={() => setCurrentCardIndex((prev) => prev - 1)}>
+          Previous
+        </button>
+        <button onClick={() => setCurrentCardIndex((prev) => prev + 1)}>
+          Next
+        </button>
       </div>
     </div>
   );
