43e428cc120e34a0a27d9a12ac1e59b96d4a2d06
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-13 03:03:22 +0000

    Use prettier on RestaurantCardStack.js

--- project/frontend/src/components/RestaurantCardStack.js
+++ project/frontend/src/components/RestaurantCardStack.js
@@ -6,22 +6,23 @@ import RestaurantCard from './RestaurantCard.js';
 
 /**
  * A navigable stack of restaurant cards.
- * 
+ *
  * @param {!Array<Object<string, *>>} props.cards A list of restaurant objects whose
  * data will be displayed in RestaurantCard components within this stack.
  */
 function RestaurantCardStack(props) {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  
+
   const maxScaleFactor = 1.0;
   const minScaleFactor = 0.85;
   const numberOfCollapsedCards = 3;
   const uncollapsedCardHeight = 487;
   const offsetBetweenCollapsedCards = 24;
   const offsetBeforeUncollapsedCard = 152;
-  const transitionString = 'top 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97), ' +
-        'opacity 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97), ' +
-        'transform 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97)';
+  const transitionString =
+    'top 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97), ' +
+    'opacity 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97), ' +
+    'transform 0.75s cubic-bezier(0.35, 0.91, 0.33, 0.97)';
   const cards = props.cards.map((card, index) => {
     if (index < currentCardIndex - 7 || index > currentCardIndex + 7) {
       return null;
@@ -29,20 +30,36 @@ function RestaurantCardStack(props) {
 
     const relativeIndex = index - currentCardIndex;
     const relativeIndexAbs = Math.abs(relativeIndex);
-    const offset = (relativeIndex === 0) ?
-          offsetBeforeUncollapsedCard : (relativeIndex > 0) ?
-          offsetBeforeUncollapsedCard + uncollapsedCardHeight +
-          Math.min(relativeIndex, numberOfCollapsedCards) * offsetBetweenCollapsedCards :
-          Math.max(numberOfCollapsedCards + relativeIndex, 0) * offsetBetweenCollapsedCards;
+    const offset =
+      relativeIndex === 0
+        ? offsetBeforeUncollapsedCard
+        : relativeIndex > 0
+        ? offsetBeforeUncollapsedCard +
+          uncollapsedCardHeight +
+          Math.min(relativeIndex, numberOfCollapsedCards) *
+            offsetBetweenCollapsedCards
+        : Math.max(numberOfCollapsedCards + relativeIndex, 0) *
+          offsetBetweenCollapsedCards;
     const top = offset + 'px';
-    const opacity = (relativeIndexAbs <= numberOfCollapsedCards) ? 1 : 0;
+    const opacity = relativeIndexAbs <= numberOfCollapsedCards ? 1 : 0;
     const zIndex = props.cards.length - relativeIndexAbs;
-    const transform = 'scale(' +
-          Math.min(Math.max(1 - relativeIndexAbs / 20, minScaleFactor), maxScaleFactor) +
-          ')';
+    const transform =
+      'scale(' +
+      Math.min(
+        Math.max(1 - relativeIndexAbs / 20, minScaleFactor),
+        maxScaleFactor
+      ) +
+      ')';
     const transition = transitionString;
-    const style = { position: 'absolute', top, opacity, zIndex, transform, transition };
-    
+    const style = {
+      position: 'absolute',
+      top,
+      opacity,
+      zIndex,
+      transform,
+      transition,
+    };
+
     return (
       <RestaurantCard
         key={card.restaurant.key.id}
@@ -56,9 +73,7 @@ function RestaurantCardStack(props) {
 
   return (
     <div className='restaurant-card-stack'>
-      <div className='restaurant-card-stack-wrapper'>
-        {cards}
-      </div>
+      <div className='restaurant-card-stack-wrapper'>{cards}</div>
       <div className='control-container'>
         <button onClick={() => setCurrentCardIndex((prev) => prev - 1)}>
           Previous
