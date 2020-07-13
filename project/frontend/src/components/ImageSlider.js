91878ad23a3a5d10dfe5de186daf0917cd3b7cc2
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-13 16:55:59 +0000

    Add todo to move reducer to new file

--- project/frontend/src/components/ImageSlider.js
+++ project/frontend/src/components/ImageSlider.js
@@ -26,6 +26,7 @@ const Action = {
  */
 const width = 344;
 
+// TODO: Move reducer to its own file.
 /**
  * A reducer for the ImageSlider component. Updates ImageSlider state
  * based on the previous state and a specified action.
