901ee24c3f57c4310fd45c70c8cbd959ffbf8165
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-14 18:08:48 +0000

    Add effect to fetch details of restaurants

--- project/frontend/src/pages/MatchResultsPage.js
+++ project/frontend/src/pages/MatchResultsPage.js
@@ -1,41 +1,60 @@
 import React, { useState, useEffect } from 'react';
 
 import getRecommendation from '../scripts/recommendation_script.js';
+import MapContainer from '../components/ResultsMap.js';
+import Modal from '../components/Modal.js';
+import RestaurantCardStack from '../components/RestaurantCardStack.js';
 
-async function fetchDetails(placeIds) {
-  const responses = await Promise.all(
-    placeIds.map(
-      (placeId) =>
-        'https://maps.googleapis.com/maps/api/place/details/json?place_id=' +
-        placeId +
-        '&key=' +
-        process.env.REACT_APP_GOOGLE_API_KEY
-    )
-  );
+async function fetchDetails(restaurants) {
+  const batchSize = 5;
+  for (let i = 0; i < restaurants.length; i += batchSize) {
+    const placeIds = restaurants.slice(i, i + batchSize).map((el) => el.restaurant.place_id);
+    const responses = await Promise.all(
+      placeIds.map(
+        (placeId) =>
+          'https://maps.googleapis.com/maps/api/place/details/json?place_id=' +
+          placeId +
+          '&key=' +
+          process.env.REACT_APP_GOOGLE_API_KEY
+      )
+    );
 
-  const data = await Promise.all(responses.map((response) => response.json()));
-  console.log(data);
-  return data;
+    const data = await Promise.all(responses.map((response) => response.json()));
+    console.log(data);
+  }
 }
 
 function MatchResultsPage(props) {
-  // const { data } = this.props.location;
-  console.log(props.location.state);
+  const formState = props.location.state;
 
-  const [restaurants, setRestaurants] = useState(null);
+  const [restaurants, setRestaurants] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getRecommendation(props.location.state, (result) => {
-      setRestaurants(result);
+    getRecommendation(formState, (result) => {
+      const restaurants = result.map((res) => ({restaurant: res, details: null}));
+      setRestaurants(restaurants);
+      fetchDetails(restaurants);
     });
   }, []);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  } else {
-    return <p>Loaded!</p>;
-  }
+  return [
+    <div key='match-results' className='container'>
+      <div className='row'>
+        <div className='one-half column'>
+          <RestaurantCardStack cards={restaurants} />
+        </div>
+        <div className='one-half column'>
+          <MapContainer />
+        </div>
+      </div>
+    </div>,
+    <Modal key='loading-modal' open={loading} center={true}>
+      <div style={{height: '300px', width: '300px'}}>
+        <p>Hi there your results are loading...</p>
+      </div>
+    </Modal>
+  ];
 }
 
 export default MatchResultsPage;
