c0d6392cc5333039057ce824589650dfb8de4981
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-14 16:48:41 +0000

    Use prettier

--- project/frontend/src/pages/MatchResultsPage.js
+++ project/frontend/src/pages/MatchResultsPage.js
@@ -3,12 +3,15 @@ import React, { useState, useEffect } from 'react';
 import getRecommendation from '../scripts/recommendation_script.js';
 
 async function fetchDetails(placeIds) {
-  const responses = await Promise.all(placeIds.map(
-    (placeId) => 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' +
-      placeId +
-      '&key=' +
-      process.env.REACT_APP_GOOGLE_API_KEY
-  ));
+  const responses = await Promise.all(
+    placeIds.map(
+      (placeId) =>
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=' +
+        placeId +
+        '&key=' +
+        process.env.REACT_APP_GOOGLE_API_KEY
+    )
+  );
 
   const data = await Promise.all(responses.map((response) => response.json()));
   console.log(data);
