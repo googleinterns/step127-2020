0749c06cfccc93542c16b6f63bbb7d90fe8638f2
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-14 23:51:30 +0000

    Use prettier on PlacesApiContext.js

--- project/frontend/src/contexts/PlacesApiContext.js
+++ project/frontend/src/contexts/PlacesApiContext.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const PlacesApiContext = React.createContext(
-  new window.google.maps.places.PlacesService(document.getElementById('attributions'))
+  new window.google.maps.places.PlacesService(
+    document.getElementById('attributions')
+  )
 );
 
 export default PlacesApiContext;
