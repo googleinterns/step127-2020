fa3f192e3fa7988cfe0a5efdcb967ba8f312665f
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-14 23:38:15 +0000

    Add a PlacesApiContext context with reference to Google PlacesService

---
+++ project/frontend/src/contexts/PlacesApiContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlacesApiContext = React.createContext(
+  new window.google.maps.places.PlacesService(document.getElementById('attributions'))
+);
+
+export default PlacesApiContext;
