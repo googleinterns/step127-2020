3ccba43f28e33a974963f805c2425a03533bd402
Kira Pancha <kirakpancha@gmail.com>
2020-07-17 09:04:52 -0400

    Change alert message

--- project/frontend/src/components/LocationFinder.js
+++ project/frontend/src/components/LocationFinder.js
@@ -17,7 +17,7 @@ class LocationFinder extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   }
 
-  /** 
+  /**
    * Uses Geolocation to get the user's current coordinates.
    * Gets formatted address to display from Geocoder.
    */
@@ -26,7 +26,10 @@ class LocationFinder extends React.Component {
       const google = window.google;
       const geocoder = new google.maps.Geocoder();
       navigator.geolocation.getCurrentPosition((position) => {
-        const currLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
+        const currLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
         geocoder.geocode({ location: currLocation }, (results, status) => {
           if (status === 'OK') {
             const locationName = results[0].formatted_address;
@@ -34,12 +37,18 @@ class LocationFinder extends React.Component {
             this.setState({ submitted: true });
             this.props.sendData({ currLocation, locationName });
           } else {
-            alert('Geocode was not successful for the following reason: ' + status);
+            alert(
+              'Geocode was not successful for the following reason: ' +
+                status +
+                '. Try entering a location in the text box below.'
+            );
           }
         });
       });
     } else {
-      alert('Cannot find your location');
+      alert(
+        'Cannot find your location. Try entering a location in the text box below.'
+      );
     }
   }
 
@@ -52,10 +61,10 @@ class LocationFinder extends React.Component {
     const geocoder = new google.maps.Geocoder();
     geocoder.geocode({ address: this.state.userInput }, (results, status) => {
       if (status === 'OK') {
-        const currLocation = { 
-          lat: results[0].geometry.location.lat(), 
+        const currLocation = {
+          lat: results[0].geometry.location.lat(),
           lng: results[0].geometry.location.lng(),
-         };
+        };
         const locationName = results[0].formatted_address;
         this.setState({ locationName });
         this.setState({ submitted: true });
@@ -69,7 +78,9 @@ class LocationFinder extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.getLocationFromGeolocate}>Use My Current Location</button>
+        <button onClick={this.getLocationFromGeolocate}>
+          Use My Current Location
+        </button>
         <form id='get-input-location-form' onSubmit={this.getLocationFromText}>
           <input name='userInput' onChange={this.changeState} />
           <button type='submit'>Find Location</button>
