0ee69e59de427f1f1009a8e81dc9400be9fcde9b
Kira Pancha <kirakpancha@gmail.com>
2020-07-17 15:56:17 -0400

    Add error function

--- project/frontend/src/components/LocationFinder.js
+++ project/frontend/src/components/LocationFinder.js
@@ -8,11 +8,14 @@ class LocationFinder extends React.Component {
       userInput: '',
       locationName: '',
       submitted: false,
+      error: false,
       autocomplete: null,
     };
     this.changeState = this.changeState.bind(this);
     this.getLocationFromGeolocate = this.getLocationFromGeolocate.bind(this);
     this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+    this.handleGeolocateSuccess = this.handleGeolocateSuccess.bind(this);
+    this.handleGeolocateError = this.handleGeolocateError.bind(this);
   }
 
   componentDidMount() {
@@ -32,6 +35,7 @@ class LocationFinder extends React.Component {
     };
     this.setState({ locationName });
     this.setState({ submitted: true });
+    this.setState({ error: false });
     this.props.sendData({ currLocation, locationName });
   }
 
@@ -45,32 +49,51 @@ class LocationFinder extends React.Component {
    */
   getLocationFromGeolocate() {
     if (navigator.geolocation) {
-      const google = window.google;
-      const geocoder = new google.maps.Geocoder();
-      navigator.geolocation.getCurrentPosition((position) => {
-        const currLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        geocoder.geocode({ location: currLocation }, (results, status) => {
-          if (status === 'OK') {
-            const locationName = results[0].formatted_address;
-            this.setState({ locationName });
-            this.setState({ submitted: true });
-            this.props.sendData({ currLocation, locationName });
-          } else {
-            alert(
-              'Geocode was not successful for the following reason: ' +
-                status +
-                '. Try entering a location in the text box below.'
-            );
-          }
-        });
-      });
+      navigator.geolocation.getCurrentPosition(this.handleGeolocateSuccess, this.handleGeolocateError);
     } else {
       alert(
         'Cannot find your location. Try entering a location in the text box below.'
       );
+    }
+  }
+
+  handleGeolocateSuccess(position) {
+    const google = window.google;
+    const geocoder = new google.maps.Geocoder();
+    const currLocation = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    geocoder.geocode({ location: currLocation }, (results, status) => {
+      if (status === 'OK') {
+        const locationName = results[0].formatted_address;
+        this.setState({ locationName });
+        this.setState({ submitted: true });
+        this.setState({ error: false });
+        this.props.sendData({ currLocation, locationName });
+      } else {
+        this.setState({ error: true });
+        const result = document.getElementById('error-label');
+        result.innerHTML =
+          'Geocode was not successful for the following reason: ' +
+            status +
+            '. Try entering a location in the text box below.'
+        ;
+      }
+    });
+  }
+
+  handleGeolocateError(error) {
+    this.setState({ error: true });
+    const result = document.getElementById('error-label');
+    if(error.code === 1) {
+      result.innerHTML = "You've decided not to share your position, but it's OK. We won't ask you again.";
+    } else if(error.code === 2) {
+      result.innerHTML = "The network is down or the positioning service can't be reached.";
+    } else if(error.code === 3) {
+      result.innerHTML = "The attempt timed out before it could get the location data.";
+    } else {
+      result.innerHTML = "Geolocation failed due to unknown error.";
     }
   }
 
@@ -86,8 +109,11 @@ class LocationFinder extends React.Component {
         <button onClick={this.getLocationFromGeolocate}>
           <MyLocation />
         </button>
+        {this.state.error ? (
+          <p id='error-label' className='highlighted-label' />
+        ) : null}
         {this.state.submitted ? (
-          <p id='location-label'>
+          <p id='location-label'className='highlighted-label' >
             Your current location: {this.state.locationName}
           </p>
         ) : null}
