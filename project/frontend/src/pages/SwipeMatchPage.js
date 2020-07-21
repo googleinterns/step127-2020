6229e0a177a147912e460343045f1325d44d02c0
Alexander Costa <alexcostaluiz@outlook.com>
2020-07-21 06:22:25 +0000

    Create SwipeMatchPage component

---
+++ project/frontend/src/pages/SwipeMatchPage.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+function SwipeMatchPage(props) {
+  const username = props.location.state.username;
+  
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource('/api/swipematch');
+    
+    eventSource.addEventListener('userconnect', (event) => {
+      console.log(event.data);
+      setUsers((prev) => [...prev, event.data]);
+    });
+
+    fetch('/api/swipematch', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'username=' + username,
+    });
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => <span>user</span>)}
+    </div>
+  );
+}
