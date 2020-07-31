636c1b7e6cb879894797c80b0cd3174118e232b3
Miah Sanchez <miahsanchez@google.com>
2020-07-30 21:20:02 -0400

    changed styling to be the profile

--- project/frontend/src/components/ProfileForm.js
+++ project/frontend/src/components/ProfileForm.js
@@ -1,11 +1,9 @@
 import './ProfileForm.css';
 
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
 
 import CuisineAutocomplete from './CuisineAutocomplete.js';
-import { Slider } from 'rsuite';
 import { useHistory } from 'react-router-dom';
-import { AuthContext } from './Authentication.js';
 
 import Place from '../assets/place.svg';
 import Cuisine from '../assets/cuisine.svg';
@@ -23,64 +21,21 @@ import Price from '../assets/dollar.svg';
  */
 function ProfileForm(props) {
   const history = useHistory();
-  const authContext = useContext(AuthContext);
 
-  const [cuisineOptions, setCuisineOptions] = useState([
-    'Italian',
-    'Mexican',
-    'Indian',
-  ]);
+  const cuisineOptions = ['Italian', 'Mexican', 'Indian'];
   const [cuisine, setCuisine] = useState([]);
-  const [radius, setRadius] = useState('');
-  const [radiusWeight, setRadiusWeight] = useState(3);
-  const [diningExp, setDiningExp] = useState('');
-  const [diningExpWeight, setDiningExpWeight] = useState(3);
-  const [priceLevel, setPriceLevel] = useState('');
-  const [priceLevelWeight, setPriceLevelWeight] = useState(3);
-  const [open, setOpen] = useState(true);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    history.push({
-      pathname: '/match-results',
-      state: {
-        cuisine,
-        radius: { pref: radius, weight: radiusWeight },
-        diningExp: { pref: diningExp, weight: diningExpWeight },
-        priceLevel: { pref: priceLevel, weight: priceLevelWeight },
-        open,
-        cuisineOptions,
-      },
-    });
-  };
-
-  const getSelect = (name, value, setValue, options) => {
-    const onChange = (event) => {
-      const value = event.target.value;
-      setValue(event.target.name === 'diningExp' ? value : parseInt(value));
-    };
-
-    return (
-      <select name={name} className='pref' value={value} onChange={onChange}>
-        <option label='Select...' key='default' value={''} />
-        {Object.entries(options).map(([label, value]) => (
-          <option label={label} key={label} value={value} />
-        ))}
-      </select>
-    );
-  };
 
   return (
-    <div className='preference-form-container'>
-      <form className='preference-form' onSubmit={handleSubmit}>
+    <div className='profile-form-container'>
+      <form className='profile-form'>
         <h4>Your profile.</h4>
-        <div className='preference-form-row'>
+        <div className='profile-form-row'>
           <img src={Place} alt='' />
           <label>Location</label>
           {/* TODO: change so that if a location isn't inputted then we are going to need to have a fill in optiob. */}
           <p>Your location</p>
         </div>
-        <div className='preference-form-row'>
+        <div className='profile-form-row'>
           <img src={Cuisine} alt='' />
           <label htmlFor='cuisine'>Preffered Cuisines</label>
           <CuisineAutocomplete
@@ -88,7 +43,7 @@ function ProfileForm(props) {
             setCuisine={setCuisine}
           />
         </div>
-        <div className='preference-form-row'>
+        <div className='profile-form-row'>
           <img src={Distance} alt='' />
           <label htmlFor='distance'>Ideal Distance</label>
           <div class='btn-group'>
@@ -98,7 +53,7 @@ function ProfileForm(props) {
             <button>$$$$</button>
           </div>
         </div>
-        <div className='preference-form-submit-container'>
+        <div className='profile-form-submit-container'>
           <button type='submit'>Update Profile</button>
         </div>
       </form>
--- project/frontend/src/pages/ProfilePage.js
+++ project/frontend/src/pages/ProfilePage.js
@@ -1,5 +1,4 @@
 import ProfileForm from '../components/ProfileForm.js';
-import PreferenceForm from '../components/PreferenceForm';
 import React from 'react';
 
 function ProfilePage(props) {
