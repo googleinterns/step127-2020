461e8fd4b22faa0bb3a0c27403042524ab2e4c6f
Miah Sanchez <miahsanchez@google.com>
2020-07-31 09:58:55 -0400

    added ButtonGroup code

---
+++ project/frontend/src/components/ButtonGroup.js
@@ -0,0 +1,15 @@
+import './ButtonGroup.css';
+
+import React from 'react';
+
+function ButtonGroup(props) {
+  // TODO: Add spec.
+  const { labelList } = props;
+  const buttonList = [];
+  for (let delta = 0; delta < labelList.length; delta++) {
+    buttonList.push(<button>{labelList[delta]}</button>);
+  }
+  return <div className='btn-group'>{buttonList}</div>;
+}
+
+export default ButtonGroup;
