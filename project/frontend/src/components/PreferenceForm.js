2ad4d941af99d3a164d5036dcd55980fb55161b3
Miah Sanchez <miahsanchez@google.com>
2020-08-03 14:20:02 -0400

    moved all labels to form

--- project/frontend/src/components/PreferenceForm.js
+++ project/frontend/src/components/PreferenceForm.js
@@ -1,13 +1,43 @@
-import React from 'react';
 import './PreferenceForm.css';
+import './FormItem.js';
+import React, { useState } from 'react';
+
+import CuisineAutocomplete from './CuisineAutocomplete.js';
+import FormItem from './FormItem.js';
+
+import Place from '../assets/place.svg';
+import Cuisine from '../assets/cuisine.svg';
+import Distance from '../assets/distance.svg';
+import Experience from '../assets/food_service.svg';
+import Price from '../assets/dollar.svg';
 
 function PreferenceForm(props) {
   const { headerLabel } = props;
+  const cuisineOptions = ['Italian', 'Mexican'];
+  const [cuisine, setCuisine] = useState([]);
   return (
     <div className='form-container'>
       <form className='form'>
         <h4 className='header'>{headerLabel}</h4>
-        {props.children}
+        <FormItem imageName={Place} label='Your Location'>
+          <p>Your Location</p>
+        </FormItem>
+        <FormItem imageName={Cuisine} label='PreferredCuisine'>
+          <CuisineAutocomplete
+            cuisineOptions={cuisineOptions}
+            setCuisine={setCuisine}
+          />
+        </FormItem>
+        <FormItem imageName={Distance} label='Ideal Distance'>
+          {/** The input types are different for the distance, price and experience so I made this props.children */}
+          {props.children[0]}
+        </FormItem>
+        <FormItem imageName={Price} label='Price Level'>
+          {props.children[1]}
+        </FormItem>
+        <FormItem imageName={Experience} label='Dining Experience'>
+          {props.children[2]}
+        </FormItem>
         <div className='form-submit-container'>
           <button type='submit'>Update Profile</button>
         </div>
--- project/frontend/src/components/ProfileForm.js
+++ project/frontend/src/components/ProfileForm.js
@@ -1,15 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import CuisineAutocomplete from './CuisineAutocomplete.js';
 import ButtonGroup from './ButtonGroup.js';
 import PreferenceForm from './PreferenceForm.js';
-import FormItem from './FormItem.js';
-
-import Place from '../assets/place.svg';
-import Cuisine from '../assets/cuisine.svg';
-import Distance from '../assets/distance.svg';
-import Experience from '../assets/food_service.svg';
-import Price from '../assets/dollar.svg';
 
 /**
  * A form used to gather the restaurant preferences of a user for the 'Find My Match'
@@ -20,32 +12,11 @@ import Price from '../assets/dollar.svg';
  * @param {string} locationName The formatted address of the current user.
  */
 function ProfileForm(props) {
-  const cuisineOptions = ['Italian', 'Mexican', 'Indian'];
-  const [cuisine, setCuisine] = useState([]);
-
   return (
     <PreferenceForm headerLabel='Your Profile'>
-      {/* TODO: change so that if a location isn't inputted then we are going to need to have a fill in optiob. */}
-      <FormItem imageName={Place} label='Your Location'>
-        <p>Your Location</p>
-      </FormItem>
-      <FormItem imageName={Cuisine} label='PreferredCuisine'>
-        <CuisineAutocomplete
-          cuisineOptions={cuisineOptions}
-          setCuisine={setCuisine}
-        />
-      </FormItem>
-      <FormItem imageName={Distance} label='Ideal Distance'>
-        <ButtonGroup
-          labelList={['1 mile', '5 miles', '10 miles', '25 miles']}
-        />
-      </FormItem>
-      <FormItem imageName={Price} label='Price Level'>
-        <ButtonGroup labelList={['$', '$$', '$$$', '$$$$']} />
-      </FormItem>
-      <FormItem imageName={Experience} label='Dining Experience'>
-        <ButtonGroup labelList={['Takeout', 'Delivery', 'Dine-In']} />
-      </FormItem>
+      <ButtonGroup labelList={['1 mile', '5 miles', '10 miles', '25 miles']} />
+      <ButtonGroup labelList={['$', '$$', '$$$', '$$$$']} />
+      <ButtonGroup labelList={['Takeout', 'Delivery', 'Dine-In']} />
     </PreferenceForm>
   );
 }
