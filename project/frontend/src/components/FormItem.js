1deec95e99d26040cc9e9b54bd5f5ebc9167c07f
Miah Sanchez <miahsanchez@google.com>
2020-08-03 13:02:42 -0400

    added alt TODO

--- project/frontend/src/components/FormItem.js
+++ project/frontend/src/components/FormItem.js
@@ -5,6 +5,7 @@ function FormItem(props) {
   const { label, imageName } = props;
   return (
     <div className='row'>
+      {/*TODO: add an alt prop to be passed in here*/}
       <img src={imageName} alt='' />
       <label>{label}</label>
       {props.children}
