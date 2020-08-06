6c0a50ded96129b87e2300a661feaf6221d2c0e3
Alexander Costa <alexcostaluiz@outlook.com>
2020-08-06 21:52:31 +0000

    Create SwipeMatchLeaderboard component

---
+++ project/frontend/src/components/SwipeMatchLeaderboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import FlipMove from 'react-flip-move';
+import RestaurantRankingView from '../components/RestaurantRankingView.js';
+
+function SwipeMatchLeaderboard(props) {
+  const { restaurants } = props;
+  
+  return (
+    <FlipMove
+      easing='cubic-bezier(0.35, 0.91, 0.33, 0.97)'
+      staggerDurationBy={15}
+      staggerDelayBy={20}>
+      {[...restaurants].sort((r1, r2) => r2.fractionLiked - r1.fractionLiked).map(
+        (restaurant, index) => {
+          if (index > 4) return null;
+          return (
+            <RestaurantRankingView
+              key={restaurant.key.id}
+              restaurant={restaurant}
+            />
+          );
+        }
+      )}
+    </FlipMove>
+  );
+}
+
+export default SwipeMatchLeaderboard;
