5dd62e8ffdeffdd302bcea4bbca5772e60e1995e
Alexander Costa <alexcostaluiz@outlook.com>
2020-08-06 22:19:37 +0000

    Use prettier on SwipeMatchLeaderboard.js

--- project/frontend/src/components/SwipeMatchLeaderboard.js
+++ project/frontend/src/components/SwipeMatchLeaderboard.js
@@ -4,14 +4,15 @@ import RestaurantRankingView from '../components/RestaurantRankingView.js';
 
 function SwipeMatchLeaderboard(props) {
   const { restaurants } = props;
-  
+
   return (
     <FlipMove
       easing='cubic-bezier(0.35, 0.91, 0.33, 0.97)'
       staggerDurationBy={15}
       staggerDelayBy={20}>
-      {[...restaurants].sort((r1, r2) => r2.fractionLiked - r1.fractionLiked).map(
-        (restaurant, index) => {
+      {[...restaurants]
+        .sort((r1, r2) => r2.fractionLiked - r1.fractionLiked)
+        .map((restaurant, index) => {
           if (index > 4) return null;
           return (
             <RestaurantRankingView
@@ -19,8 +20,7 @@ function SwipeMatchLeaderboard(props) {
               restaurant={restaurant}
             />
           );
-        }
-      )}
+        })}
     </FlipMove>
   );
 }
