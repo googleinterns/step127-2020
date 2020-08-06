f7128a11f8dcde368751d2a0cb1b9fc06ab941da
Miah Sanchez <miahsanchez@google.com>
2020-08-06 15:04:40 -0400

    merged preferenceFormComponent

--- project/frontend/src/components/ProfileForm.js
+++ project/frontend/src/components/ProfileForm.js
@@ -1,15 +1,8 @@
+import React from 'react';
 import './ProfileForm.css';
 
-import React, { useState } from 'react';
-
-import CuisineAutocomplete from './CuisineAutocomplete.js';
-import ButtonGroup from './ButtonGroup.js';
-
-import Place from '../assets/place.svg';
-import Cuisine from '../assets/cuisine.svg';
-import Distance from '../assets/distance.svg';
-import Experience from '../assets/food_service.svg';
-import Price from '../assets/dollar.svg';
+import PreferenceForm from './PreferenceForm.js';
+import ButtonGroup from './ButtonGroup';
 
 /**
  * A form used to gather the restaurant preferences of a user for the 'Find My Match'
@@ -20,48 +13,40 @@ import Price from '../assets/dollar.svg';
  * @param {string} locationName The formatted address of the current user.
  */
 function ProfileForm(props) {
-  const cuisineOptions = ['Italian', 'Mexican', 'Indian'];
-  const [cuisine, setCuisine] = useState([]);
-
+  const itemLabels = {
+    cuisine: 'Preferred Cuisine',
+    location: 'Your Location',
+    distance: 'Preferred Distance',
+    price: 'Price Level',
+    experience: 'Dining Experience',
+  };
+  const cuisineOptions = ['Italian', 'Mexican'];
+  // This is a filler value. I plan on moving all cuisine stuff to PreferenceForm.js
+  const setCuisine = () => {};
   return (
     <div className='profile-form-container'>
-      <form className='profile-form'>
-        <h4 className='profile-header'>Your profile.</h4>
-        <div className='profile-form-row'>
-          <img src={Place} alt='' />
-          <label>Your Location</label>
-          {/* TODO: change so that if a location isn't inputted then we are going to need to have a fill in optiob. */}
-          <p>Your location</p>
-        </div>
-        <div className='profile-form-row'>
-          <img src={Cuisine} alt='' />
-          <label htmlFor='cuisine'>Preferred Cuisines</label>
-          <CuisineAutocomplete
-            cuisineOptions={cuisineOptions}
-            setCuisine={setCuisine}
-          />
-        </div>
-        <div className='profile-form-row'>
-          <img src={Distance} alt='' />
-          <label htmlFor='distance'>Ideal Distance</label>
-          <ButtonGroup
-            labelList={['1 mile', '5 miles', '10 miles', '25 miles']}
-          />
-        </div>
-        <div className='profile-form-row'>
-          <img src={Price} alt='' />
-          <label htmlFor='price'>Price Level</label>
-          <ButtonGroup labelList={['$', '$$', '$$$', '$$$$']} />
-        </div>
-        <div className='profile-form-row'>
-          <img src={Experience} alt='' />
-          <label htmlFor='experience'>Dining Experience</label>
-          <ButtonGroup labelList={['Takeout', 'Delivery', 'Dine-In']} />
-        </div>
-        <div className='profile-form-submit-container'>
-          <button type='submit'>Update Profile</button>
-        </div>
-      </form>
+      <h4 className='header'>Your Profile</h4>
+      <PreferenceForm
+        headerLabel='Your Profile'
+        rowItemLabels={itemLabels}
+        locationName='Your Location'
+        buttonLabel='Update Profile'
+        cuisineOptions={cuisineOptions}
+        setCuisine={setCuisine}>
+        {/**
+         * If the order of the children here is changed this will need to be
+         * accounted for in the preference form (PreferenceForm.js).
+         */}
+        <ButtonGroup
+          id='distance'
+          labelList={['1 mile', '5 miles', '10 miles', '25 miles']}
+        />
+        <ButtonGroup id='price' labelList={['$', '$$', '$$$', '$$$$']} />
+        <ButtonGroup
+          id='experience'
+          labelList={['Takeout', 'Delivery', 'Dine-In']}
+        />
+      </PreferenceForm>
     </div>
   );
 }
