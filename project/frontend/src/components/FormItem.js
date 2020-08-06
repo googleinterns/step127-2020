902cb35e07cd6d4aa15b30a32bc48437facfe305
Miah Sanchez <miahsanchez@google.com>
2020-08-06 12:37:22 -0400

    removed alt TODO

--- project/frontend/src/components/FormItem.js
+++ project/frontend/src/components/FormItem.js
@@ -5,7 +5,6 @@ function FormItem(props) {
   const { label, imageName } = props;
   return (
     <div className='row'>
-      {/*TODO: add an alt prop to be passed in here*/}
       <img src={imageName} alt={label + ' icon'} />
       <label>{label} </label>
       {props.children}
