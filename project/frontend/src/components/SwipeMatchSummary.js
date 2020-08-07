20c7d7962f5c67aa5f9ae7c7808f8e23ebf02bc5
Alexander Costa <alexcostaluiz@outlook.com>
2020-08-07 14:51:53 +0000

    Reverse if-else tree

--- project/frontend/src/components/SwipeMatchSummary.js
+++ project/frontend/src/components/SwipeMatchSummary.js
@@ -15,66 +15,67 @@ function SwipeMatchSummary(props) {
 
   const history = useHistory();
 
+  if (restaurants.length === 0) {
+    return null;
+  }
+
   const topPick = restaurants[0];
+  const id = topPick.key.id;
+  const name = topPick.key.name;
+  const votes = topPick.fractionLiked * users.length;
+  const userLikes = likes[id];
 
-  if (topPick) {
-    const id = topPick.key.id;
-    const name = topPick.key.name;
-    const votes = topPick.fractionLiked * users.length;
-    const userLikes = likes[id];
-
-    return (
-      <div className='swipe-match-summary'>
-        <h4>
-          Your top pick was{' '}
-          {<h4 className='swipe-match-summary-name'>{name + ' '}</h4>}
-          with {votes} out of {users.length} votes!
-        </h4>
-        <div className='swipe-match-summary-card-container'>
-          <div>
-            <RestaurantCard restaurant={topPick} />
-          </div>
+  return (
+    <div className='swipe-match-summary'>
+      <h4>
+        Your top pick was{' '}
+        {<h4 className='swipe-match-summary-name'>{name + ' '}</h4>}
+        with {votes} out of {users.length} votes!
+      </h4>
+      <div className='swipe-match-summary-card-container'>
+        <div>
+          <RestaurantCard restaurant={topPick} />
         </div>
-        <table>
-          <tbody>
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <img src={ThumbUp} alt='Thumb up' />
+            </td>
+            <td>
+              <label>Likes</label>
+            </td>
+            <td>
+              {userLikes &&
+                userLikes.map((user) => (
+                  <ConnectedUserChip key={user} username={user} />
+                ))}
+            </td>
+          </tr>
+          {userLikes && userLikes.length < users.length && (
             <tr>
               <td>
-                <img src={ThumbUp} alt='Thumb up' />
+                <img src={ThumbDown} alt='Thumb down' />
               </td>
               <td>
-                <label>Likes</label>
+                <label>Dislikes</label>
               </td>
               <td>
                 {userLikes &&
-                  userLikes.map((user) => (
-                    <ConnectedUserChip key={user} username={user} />
-                  ))}
+                  users.map((user) =>
+                    !userLikes.includes(user) ? (
+                      <ConnectedUserChip key={user} username={user} />
+                    ) : null
+                  )}
               </td>
             </tr>
-            {userLikes && userLikes.length < users.length && (
-              <tr>
-                <td>
-                  <img src={ThumbDown} alt='Thumb down' />
-                </td>
-                <td>
-                  <label>Dislikes</label>
-                </td>
-                <td>
-                  {userLikes &&
-                    users.map((user) =>
-                      !userLikes.includes(user) ? (
-                        <ConnectedUserChip key={user} username={user} />
-                      ) : null
-                    )}
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        <button onClick={() => history.replace('/')}>Homepage</button>
-      </div>
-    );
-  } else return null;
+          )}
+        </tbody>
+      </table>
+      <button onClick={() => history.replace('/')}>Homepage</button>
+    </div>
+  );
 }
 
 export default SwipeMatchSummary;
