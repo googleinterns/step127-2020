dc111a73a828def304562747fc062be64ff65577
Miah Sanchez <miahsanchez@google.com>
2020-08-07 12:35:57 -0400

    fix build issues

--- project/frontend/src/contexts/FirestoreContext.js
+++ project/frontend/src/contexts/FirestoreContext.js
@@ -6,12 +6,12 @@ import 'firebase/firestore';
 function _initFirestore() {
   const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
-    authDomain: REACT_APP_FIREBASE_DATABASE_URL,
-    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_I,
-    appId: REACT_APP_FIREBASE_APP_ID,
+    authDomain: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_I,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
   firebase.initializeApp(firebaseConfig);
